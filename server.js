ca396bc27ca0a24629e6dfdb395850831fb346f1
Sara Sundblom <sundblomsara@gmail.com>
2020-12-10 12:47:14 +0100

    multiple endpoints

--- server.js
+++ server.js
@@ -1,24 +1,73 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
+import express, { request, response } from 'express'
+import csvtojson from 'csvtojson'
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
-const port = process.env.PORT || 8080
+const csvFilePath = 'cwurData.csv'
+const ERROR_NOT_FOUND = { error: 'Nothing to be found' }
+
+let rankings = []
+csvtojson()
+  .fromFile(csvFilePath)
+  .then((jsonObj) => {
+    rankings = jsonObj
+    console.log(jsonObj.length)
+  })
+
 const app = express()
 
-// Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(bodyParser.json())
-
-// Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send('morningmorning!')
 })
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`)
+app.get('/rankings', (req, res) => {
+  res.json(rankings)
 })
+
+app.get('/rankings/:name', (req, res) => {
+  const { name } = req.params
+  const filterRankings = rankings.filter((ranking) => ranking.institution === name)
+  if (filterRankings.length === 0) {
+    res.status(404).json(ERROR_NOT_FOUND)
+  } else {
+    res.json(filterRankings)
+  }
+})
+
+app.get('/rankings/:year/ranks/:rank', (req, res) => {
+  const { year, rank } = req.params
+  const ranking = rankings.find(ranking =>
+    ranking.year === year &&
+    ranking.world_rank === rank
+  )
+
+  if (!ranking) {
+    res.status(404).json(ERROR_NOT_FOUND)
+  } else {
+    res.json(ranking)
+  }
+})
+
+app.get('/rankings/:year/universities', (req, res) => {
+  const { year } = req.params
+  const { country } = req.query
+
+  let filterRankings = rankings
+
+  filterRankings = filteredRankings.filter(
+    (ranking) => +ranking.institution === +year)
+  
+  filterRankings = filteredRankings.filter(
+    (ranking) => +ranking.country === country)
+
+  if (filterRankings.length === 0) {
+    res.status(404).json(ERROR_NOT_FOUND)
+  } else {
+    res.json(filterRankings)
+  }
+
+})
+
+
+
+app.listen(8080, () => {
+  console.log('morning console!')
+}) 
